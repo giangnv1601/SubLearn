2e87cd65fbca193727e9de4c7a0f85dccc1668ad
Nguyen Van Giang <giang16012405@gmail.com>
2025-12-15 01:22:12 +0700

    feat(manage-user): add menuItem manage users

--- frontend/src/components/TaskBars/TaskBarAdmin.jsx
+++ frontend/src/components/TaskBars/TaskBarAdmin.jsx
@@ -3,26 +3,28 @@ import SearchBar from './MenuItems/SearchBar'
 import Profiles from './MenuItems/Profile'
 import ManageMovies from './MenuItems/ManageMovies'
 import ManageExercises from './MenuItems/ManageExercises'
+import ManagerUsers from './MenuItems/ManagerUsers'
 import Home from './MenuItems/Home'
 
 const TaskBarAdmin = () => {
   return (
     <div className='flex justify-between items-center p-4 bg-[#1D2732] text-white rounded-2px rounded-t-2xl'>
       {/* Logo */}
-      <LogoSubLearn/>
+        <LogoSubLearn/>
 
       {/* Menu Items Admin */}
       <div className="hidden md:flex items-center space-x-2">
         <Home />
+        <ManagerUsers/>
         <ManageMovies/>
         <ManageExercises/>
       </div>
 
       {/* Search Bar */}
-      <SearchBar/>
+        <SearchBar/>
 
       {/* User Profile */}
-      <Profiles/>
+        <Profiles/>
     </div>  
   )
 }
