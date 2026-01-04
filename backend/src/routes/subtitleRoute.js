422afbb65cf25884a7fa438d705907a3a4c8dc62
Nguyen Van Giang <giang16012405@gmail.com>
2026-01-04 08:52:37 +0700

    refactor: rft name file

--- backend/src/routes/subtitleRoute.js
+++ backend/src/routes/subtitleRoute.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { multerUploadMiddleware } from '../middlewares/multerUploadMiddleware.js';
+import { uploadMiddleware } from '../middlewares/uploadMiddleware.js';
 import { uploadSubtitle, getSubtitlesByMovie } from '../controllers/subtitleController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router()
 
-router.post('/', authMiddleware.isAuthorized, authMiddleware.isAdmin, multerUploadMiddleware.uploadSubtitle, uploadSubtitle)
+router.post('/', authMiddleware.isAuthorized, authMiddleware.isAdmin, uploadMiddleware.uploadSubtitle, uploadSubtitle)
 router.get('/:movieId', authMiddleware.isAuthorized, authMiddleware.isAdminOrClient, getSubtitlesByMovie)
 
 export default router
